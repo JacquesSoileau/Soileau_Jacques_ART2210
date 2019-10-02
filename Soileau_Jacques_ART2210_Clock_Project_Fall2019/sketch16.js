9b58c080ccbe7a5c9ce9002c7dd5f16eff4cda52
Jacques Soileau <jsoil34@lsu.edu>
2019-10-02 12:49:20 -0500

    Update sketch16.js

--- Soileau_Jacques_ART2210_Clock_Project_Fall2019/sketch16.js
+++ Soileau_Jacques_ART2210_Clock_Project_Fall2019/sketch16.js
@@ -33,15 +33,15 @@ function setup(){
 
       strokeWeight(4);
       noFill();
-      stroke(255, 0, 0);
+      stroke(0, 0, 0);
       let secondAngle = map(sc, 0, 60, 0, 360);
       arc(0, 0, 300, 300, 0, secondAngle);
 
-      stroke(0, 255, 0);
+      stroke(0, 0, 0);
       let minuteAngle = map(mn, 0, 60, 0, 360);
       arc(0, 0, 280, 280, 0, minuteAngle);
 
-      stroke(0, 0, 255);
+      stroke(0, 0, 0);
       let hourAngle = map(hr % 12, 0, 12, 0, 360);
       arc(0, 0, 260, 260, 0, hourAngle);
 
